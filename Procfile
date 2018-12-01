13c7dba7576abf15d3f44dffea80f792d1229e54
Matthieu Bourges <matthieu.bourges@hd.digital>
2018-12-01 23:55:14 +0100

    First commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: npm start
+kitten: node services/kitten
---
+++ services/kitten.js
@@ -0,0 +1,38 @@
+const throng = require('throng');
+const jackrabbit = require('jackrabbit');
+const fetch = require('node-fetch');
+
+const CONCURRENCY = process.env.WEB_CONCURRENCY || 1;
+const RABBIT_URL = process.env.CLOUDAMQP_URL || 'amqp://localhost';
+
+throng({ workers: CONCURRENCY, lifetime: Infinity }, start);
+
+function start(id) {
+  console.log({ type: 'info', message: `starting kitten service id: ${id}` });
+
+  const rabbit = jackrabbit(RABBIT_URL);
+
+  rabbit.once('connected', create);
+  process.once('uncaughtException', onError);
+
+  function create() {
+    const exchange = rabbit.default();
+    const rpc = exchange.queue({ name: "kitten.get", prefetch: 1, durable: false });
+
+    rpc.consume(onRequest);
+  }
+
+  async function onRequest(data, reply) {
+    const kittenResponse = await fetch('https://api.thecatapi.com/v1/images/search');
+    const kittenJson = await kittenResponse.json();
+    const kittenUrl = kittenJson[0].url
+
+    reply({ result: kittenUrl});
+  }
+
+  function onError(err) {
+    console.log({ type: 'error', service: 'kitten', error: err, stack: err.stack || 'No stacktrace' }, process.stderr);
+    console.log({ type: 'info', message: 'killing kitten' });
+    process.exit();
+  }
+}
